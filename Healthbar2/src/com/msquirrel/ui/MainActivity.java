75f5734c79487e34e8c736e826ba721c2c5fbbec
XXiaoMei <15757129709@163.com>
2015-05-13 09:59:22 +0800

    重新把HealthBar的布局设计了一下，主要是仿微信的朋友圈的界面。

---
+++ Healthbar2/src/com/msquirrel/ui/MainActivity.java
@@ -0,0 +1,47 @@
+package com.msquirrel.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import android.widget.ListView;
+
+import com.msquirrel.adapter.WeChatAdapter;
+
+import com.msquirrel.bean.UserInfo;
+
+public class MainActivity extends Activity {
+
+	private ListView mListView = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_healthbar);
+		mListView = (ListView) findViewById(R.id.lv_main);
+	
+		// mListView.setDividerHeight(0);
+		setData();
+	}
+
+	
+	private void setData() {
+		List<UserInfo> mList = new ArrayList();
+		UserInfo mUserInfo = new UserInfo();
+		
+		mList.add(mUserInfo);
+		//---------------------------------------------
+		UserInfo mUserInfo2 = new UserInfo();
+		
+		mList.add(mUserInfo2);	
+
+		WeChatAdapter mWeChatAdapter = new WeChatAdapter(this);
+		mWeChatAdapter.setData(mList);
+		mListView.setAdapter(mWeChatAdapter);
+	}
+
+	
+
+}
