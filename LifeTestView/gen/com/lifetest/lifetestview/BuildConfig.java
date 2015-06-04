97ca06e4541b18c5ad4f1952593d048e6ee4e847
XXiaoMei <15757129709@163.com>
2015-06-04 16:08:10 +0800

    测试部分终于完成！

---
+++ LifeTestView/gen/com/lifetest/lifetestview/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.lifetest.lifetestview;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ LifeTestView/src/com/lifetest/lifetestview/PageEntity.java
@@ -0,0 +1,33 @@
+package com.lifetest.lifetestview;
+
+import java.io.Serializable;
+
+public class PageEntity  implements Serializable{
+
+	private String title;
+	private String[] choices;
+	private int select=-1;
+	
+	public int getSelect() {
+		return select;
+	}
+	public void setSelect(int select) {
+		this.select = select;
+	}
+	public String[] getChoices() {
+		return choices;
+	}
+	public void setChoices(String... choices) {
+		this.choices = choices;
+	}
+	public PageEntity(String title){
+		this.title=title;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+}
